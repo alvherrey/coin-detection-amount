4f8d06d1f56e65eadd2a584a7523f10432a093c7
alvherrey <alvherrey@gmail.com>
2022-06-11 10:26:45 +0000

    externalize params

--- app/predict_coins.py
+++ app/predict_coins.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import numpy as np
 import base64
@@ -5,15 +6,24 @@ import base64
 def detect_coins(input_img):
 
     # defining minimal and maximal radius, specified to the coins.jpg
-    min_r = 100
-    max_r = 300
+    min_r = os.getenv('MIN_COIN_RADIO')
+    max_r = os.getenv('MAX_COIN_RADIO')
 
     # preprocessing the input image
     cv2.imwrite("euro.jpg", input_img)
 
     coins = cv2.imread('euro.jpg',0)
 
-    img = cv2.medianBlur(coins,5)
+    # print('coins:')
+    # print('--------')
+    # print(coins.dtype)
+    # print(type(coins))
+    # print(np.shape(coins))
+    # print()
+
+    img = coins.copy()
+
+    # img = cv2.medianBlur(coins,5)
 
     # get hough circles
     # circles = cv2.HoughCircles(
@@ -36,17 +46,48 @@ def detect_coins(input_img):
     #     minRadius=min_r,        # minimal radius
     #     maxRadius=max_r,        # max radius
     # )
+    
+    # circles = cv2.HoughCircles(
+    #     img,                    # source image
+    #     cv2.HOUGH_GRADIENT,     # type of detection
+    #     1,                      # always 1
+    #     300,                    # min distance between centers
+    #     param1=130,              # adjust 1
+    #     param2=60,             # adjust 2
+    #     minRadius=min_r,        # minimal radius
+    #     maxRadius=max_r,        # max radius
+    # )
+
     circles = cv2.HoughCircles(
-        img,                    # source image
-        cv2.HOUGH_GRADIENT,     # type of detection
-        1,                      # always 1
-        250,                    # min distance between centers
-        param1=130,              # adjust 1
-        param2=60,             # adjust 2
-        minRadius=min_r,        # minimal radius
-        maxRadius=max_r,        # max radius
+        img,                                # source image
+        cv2.HOUGH_GRADIENT,                 # type of detection
+        1,                                  # always 1
+        os.getenv('MIN_COIN_DISTANCE'),     # min distance between centers
+        param1=os.getenv('HOUGH_PARAM_1'),  # adjust param 1
+        param2=os.getenv('HOUGH_PARAM_2'),  # adjust param 2
+        minRadius=min_r,                    # minimal radius
+        maxRadius=max_r,                    # max radius
     )
-    circles = np.uint16(np.around(circles))
+
+    if circles is None:
+        print('es nulo')
+        return
+    
+    # print('circles:')
+    # print('--------')
+    # print(circles.dtype)
+    # print(type(circles))
+    # print(np.shape(circles))
+    # print()
+
+    circles = np.uint16(np.round(circles))
+
+    # print('circles:')
+    # print('--------')
+    # print(circles.dtype)
+    # print(type(circles))
+    # print(np.shape(circles))
+    # print()
 
     # write radios to image
     coins_copy = coins.copy()
@@ -56,10 +97,10 @@ def detect_coins(input_img):
             coins_copy,
             (int(x_coor), int(y_coor)),
             int(detected_radius),
-            (0, 255, 0),
-            4,
+            (255, 0, 0),
+            2,
         )
-        cv2.putText(coins_detected,'radio: {}'.format(detected_radius),(x_coor -120,y_coor),cv2.FONT_HERSHEY_SIMPLEX,2,(0, 0, 0), 4)
+        cv2.putText(coins_detected,'radio: {}'.format(detected_radius),(x_coor -60,y_coor),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 0, 0), 2)
     cv2.imwrite("euro_radio.jpg", coins_detected)
 
     # return circles
@@ -179,7 +220,7 @@ def calculate_amount(input_img):
         coordinates.append([x_coor, y_coor])
 
     smallest = min(radius)
-    tolerance = 0.035
+    tolerance = 0.032
     total_amount = 0
 
     coins_circled = cv2.imread('euro_radio.jpg', 1)
@@ -195,8 +236,8 @@ def calculate_amount(input_img):
             if abs(ratio_to_check - monedas[moneda]['ratio']) <= tolerance:
                 monedas[moneda]['count'] += 1
                 total_amount += monedas[moneda]['value']
-                cv2.putText(coins_circled, 'valor: {}'.format(value), (int(coor_x) -120 , int(coor_y)+ 100), font, 2,
-                            (0, 0, 255), 4)
+                cv2.putText(coins_circled, 'valor: {}'.format(value), (int(coor_x) -60 , int(coor_y)+ 30), font, 1,
+                            (0, 0, 255), 2)
 
     # Guardar imagen del resultado
     cv2.imwrite("euro_valor.jpg", coins_circled)
