924dc26980228bb2f9788c274fd5433727be1b50
alvherrey <alvherrey@gmail.com>
2022-06-11 10:31:59 +0000

    clean code

--- app/main.py
+++ app/main.py
@@ -1,18 +1,7 @@
-# from predict_coins import calculate_amount
-
-# def main():
-#     calculate_amount()
-
-# if __name__ == "__main__":
-#     main()
-
-
-
 from fastapi import FastAPI, UploadFile
 from io import BytesIO
 from PIL import Image
 import numpy as np
-import cv2
 
 from predict_coins import calculate_amount
 
@@ -20,11 +9,6 @@ app = FastAPI()
 
 def load_image_into_numpy_array(data):
     return np.array(Image.open(BytesIO(data)))
-
-# def load_image_into_numpy_array(data):
-#     npimg=np.frombuffer(data,np.uint8)
-#     frame=cv2.imdecode(npimg,cv2.IMREAD_COLOR)
-#     return cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 
 @app.post("/coins/predict")
 async def identify(file: UploadFile):
